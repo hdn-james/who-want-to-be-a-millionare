6074ad8b4e917e1a73cca6e28aed25e1b6166746
Huỳnh Đại Nhân <hdnhan18@apcs.fitus.edu.vn>
2021-11-20 16:17:06 +0700

    data demo

--- server/libserver.py
+++ server/libserver.py
@@ -5,9 +5,33 @@ import io
 import struct
 
 request_search = {
-    "morpheus": "Follow the white rabbit. \U0001f430",
-    "ring": "In the caves beneath the Misty Mountains. \U0001f48d",
-    "\U0001f436": "\U0001f43e Playing ball! \U0001f3d0",
+    "1": {
+        "question": "A calf is the young of which animal?",
+        "answers": [
+            "Cow",
+            "Cat",
+            "Goat",
+            "Gorilla"
+        ]
+    },
+    "2": {
+        "question": "A computer-game hero is Sonic the what?",
+        "answers": [
+            "Hedgehog",
+            "Polecat",
+            "Aardvark",
+            "Muskrat"
+        ]
+    },
+    "3": {
+        "question": "A hare in which month is said to be mad?",
+        "answers": [
+            "March",
+            "July",
+            "April",
+            "August"
+        ]
+    }
 }
 
 
